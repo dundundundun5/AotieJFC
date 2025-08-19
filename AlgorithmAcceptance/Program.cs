99bcec58d6fc1e9dbf57b7fbafeffea769b21472
dundundundun5 <1053450626@qq.com>
2025-08-19 14:36:08 +0800

    Revert "三合一全新版本，详见README"
    This reverts commit 2d40b81a0e15ae750c6479c5ea870805782184dc.

--- AlgorithmAcceptance/Program.cs
+++ AlgorithmAcceptance/Program.cs
@@ -1,25 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AlgorithmAcceptanceTool
+namespace AlgorithmAcceptance
 {
-    class Program
+	static class Program
 	{
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
-		
 		[STAThread]
-		public static void Main()
+		static void Main()
 		{
-			
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Main());
-			// Application.Run(new Segment());
-			//Application.Run(new OCR);
-			// Application.Run(new RiskDetect());
-
+			Application.Run(new MainForm());
 		}
 	}
 }
